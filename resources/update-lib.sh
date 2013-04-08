d175630c37f972b39bd981aa36a7a07174a25d67
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2013-04-08 16:30:58 +0400

    Update/reorganize character.

--- resources/update-lib.sh
+++ resources/update-lib.sh
@@ -3,10 +3,8 @@
 PREFIX=`dirname "$0"`
 
 for ELEMENT in \
-	foot-male-01 \
-	hand-male-01 \
-	foot-female-01 \
-	hand-female-01
+	foots \
+	hands
 do
 	if [ -e $PREFIX/${ELEMENT}.sif ]; then
 		if ( ! diff <(md5sum $PREFIX/${ELEMENT}.sif | awk '{print $1}') <(md5sum $PREFIX/${ELEMENT}-c.sif | awk '{print $1}') >/dev/null ); then
